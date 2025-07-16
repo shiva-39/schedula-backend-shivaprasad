483027cb154c1a9b245750c976192eb4679a1f5e
epic <shivaprasad392005@gmail.com>
2025-07-16 21:42:46 +0530

    Fix: remove duplicate code in app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,15 +10,3 @@ export class AppController {
     return this.appService.getHello();
   }
 }
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get('hello') // GET /hello
-  getHello(): {message: string} {
-    return this.appService.getHello();
-  }
-}
